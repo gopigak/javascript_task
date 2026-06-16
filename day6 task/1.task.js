a10ac798343665ce3f950c73ddc1809d21721d00
gopigak <gopiga@gmail.com>
2026-06-16 13:38:23 +0530

    task day 5 completed

---
+++ day6 task/1.task.js
@@ -0,0 +1,5 @@
+const nums = [10, 20, 30];
+
+const result = nums.map(num => num );
+
+console.log(result);``
---
+++ day6 task/2.task.js
@@ -0,0 +1,11 @@
+const employee = {
+    employeeName: "Gopiga",
+    department: "Frontend Developer",
+    salary: 25000,
+    city: "Chennai"
+};
+
+console.log("Name:", employee.employeeName);
+console.log("Department:", employee.department);
+console.log("Salary:", employee.salary);
+console.log("City:", employee.city);
